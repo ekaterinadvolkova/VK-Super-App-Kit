452472b2f5838f265c386f36a0e702ff1c5a7861
Kateridzhe <ekaterina.d.volkova@gmail.com>
2022-03-27 21:27:57 +0300

    Initial Commit

---
+++ VK-Super-App-Kit-2022/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VK-Super-App-Kit-2022
+//
+//  Created by Ekaterina Volkova on 27/03/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
