9556731548f08e2da96d14adc9864e635fd1a18f
Kateridzhe <ekaterina.d.volkova@gmail.com>
2022-03-27 22:34:40 +0300

    Create a table

--- VK-Super-App-Kit-2022/ViewController.swift
+++ VK-Super-App-Kit-2022/ViewController.swift
@@ -7,10 +7,30 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet var table: UITableView!
+    
+    var models = [CellContent]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        table.delegate = self
+        table.dataSource = self
     }
+    
+    //Table
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return models.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+}
+
+struct CellContent {
+    
 }
 
